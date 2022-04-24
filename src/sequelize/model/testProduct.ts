bcf0ddbeba215f1cf566d2f6266b64701d50a138
Ohjin Kwon <qpqpq13579@gmail.com>
2022-04-24 21:35:12 +0900

    Add code, discountRate column

--- src/sequelize/model/testProduct.ts
+++ src/sequelize/model/testProduct.ts
@@ -9,13 +9,22 @@ export const TestProduct = sequelize.define(
       type: DataTypes.STRING(50),
       allowNull: false,
     },
+    code: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    discountRate: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
     price: {
       type: DataTypes.INTEGER,
-      // allowNull defaults to true
+      allowNull: false,
     },
     category: {
-      type: DataTypes.ENUM("other", "ring", "necklace", "earring"),
-      allowNull: false,
+      type: DataTypes.ENUM,
+      values: ["other", "ring", "necklace", "earring"],
       defaultValue: "other",
     },
   },
